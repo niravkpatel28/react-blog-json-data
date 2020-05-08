ef1fa37b82d30daecca749f80d575c47aa4e69da
Nirav Patel <niravkpatel28@gmail.com>
2020-05-08 23:13:46 +0530

    Updates

---
+++ precache-manifest.3f6b388d5dfd1014dedcb5da142fc619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f06fdf19a851f8166e5cdc7ee12047",
+    "url": "/react-blog-json-data/index.html"
+  },
+  {
+    "revision": "bbc2208d5e546fd85756",
+    "url": "/react-blog-json-data/static/css/main.b6652379.chunk.css"
+  },
+  {
+    "revision": "b4da6b0d9d99a6e310a0",
+    "url": "/react-blog-json-data/static/js/2.2cf89fd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-blog-json-data/static/js/2.2cf89fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc2208d5e546fd85756",
+    "url": "/react-blog-json-data/static/js/main.0219a2d2.chunk.js"
+  },
+  {
+    "revision": "f21defdd0ca505410687",
+    "url": "/react-blog-json-data/static/js/runtime-main.c24e3bf2.js"
+  },
+  {
+    "revision": "498fb18a0c31dcdd3de5f845b8735420",
+    "url": "/react-blog-json-data/static/media/banner.498fb18a.png"
+  }
+]);
